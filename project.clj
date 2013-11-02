f1fd960e497b8eae08f694d4f8eed5c0e32396ea
Eduardo Díaz <iamedu@gmail.com>
2013-11-01 22:21:31 -0600

    Initial deps

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject apoya "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [fortress-ring-adapter "0.1.0-SNAPSHOT"]]
+  :main ^:skip-aot apoya.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/apoya/core.clj
@@ -0,0 +1,7 @@
+(ns apoya.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
