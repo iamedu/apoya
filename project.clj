a4a40d3cd9a3f3d52ca2b376e13dd4e2f1409abd
Eduardo Díaz <iamedu@gmail.com>
2013-11-18 16:28:54 -0600

    Upgradint clojurescript version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2014"]
+                 [org.clojure/clojurescript "0.0-2030"]
                  [org.clojure/core.cache "0.6.3"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
                  [org.clojure/tools.cli "0.2.4"]
