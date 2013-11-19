27657287974126cfb2e15043737cdbbb5498490b
Eduardo Díaz <iamedu@gmail.com>
2013-11-19 01:17:32 -0600

    Guarantee loading order
    Connection must be compiled before handler so we require it form handler

--- src/clj/sun/net/www/protocol/jclouds/handler.clj
+++ src/clj/sun/net/www/protocol/jclouds/handler.clj
@@ -1,5 +1,6 @@
 (ns sun.net.www.protocol.jclouds.handler
-  (:require [org.jclouds.blobstore2 :as bs])
+  (:require [org.jclouds.blobstore2 :as bs]
+            [sun.net.www.protocol.jclouds.connection])
   (:import sun.net.www.protocol.jclouds.JCloudsConnection)
   (:gen-class
     :name sun.net.www.protocol.jclouds.Handler
