8c723f024f6c76ad249480a29da346ee77b3b985
Eduardo Díaz <iamedu@gmail.com>
2013-11-20 16:46:53 -0600

    Add generation of application code

---
+++ release-app.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+current_version=$(cat app-version)
+last_commit=$(git rev-parse HEAD)
+meta_file="{:commit \"${last_commit}\" :version \"${current_version}\"}"
+
+rm -rf app-dist
+mkdir app-dist
+
+cp APP_CHANGELOG.md app-dist
+echo ${meta_file} > app-dist/platform-meta.edn
+
+cp -r data/fs/* app-dist
+rm -rf data/fs/sites/*/bower_components
+
+jar cfv app-${current_version}.far -C app-dist .
+
+rm -rf app-dist
+
